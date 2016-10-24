ed6a86b4a273f1031a8959edff9db80bb6794739
Henrique Smoco <henriquesmoco@outlook.com>
2016-10-24 05:51:38 -0200

    Removed unused imports

--- localflix-web/src/main/java/com/github/henriquesmoco/localflix/web/MainController.java
+++ localflix-web/src/main/java/com/github/henriquesmoco/localflix/web/MainController.java
@@ -1,12 +1,8 @@
 package com.github.henriquesmoco.localflix.web;
 
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class MainController {
